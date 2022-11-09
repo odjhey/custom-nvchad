9a67339c5223f18eca4e0bf4b0ff75c7ba57eb53
Odelon Jhey Pacalso <odjhey@gmail.com>
2022-11-09 19:43:11 +0800

    sync, delete some and enable vim-go and go lsp

--- chadrc.lua
+++ chadrc.lua
@@ -1,10 +1,29 @@
+-- Just an example, supposed to be placed in /lua/custom/
+
 local M = {}
 
--- Install plugins
-local userPlugins = require "custom.plugins" -- path to table
+-- make sure you maintain the structure of `core/default_config.lua` here,
+-- example of changing theme:
+
+M.options = {
+   user = function()
+      vim.opt.clipboard = nil
+   end,
+}
+
+M.mappings = {
+  user = {
+    n = {
+     ["<left>"]  = {"<cmd> cprev <cr>", ""},
+     ["<right>"] = {"<cmd> cnext <cr>", ""},
+     ["<up>"]    = {"<cmd> lnext <cr>", ""},
+     ["<down>"]  = {"<cmd> lprev <cr>", ""},
+    }
+  }
+}
 
 M.plugins = {
-   install = userPlugins,
+  user = require "custom.plugins",
 }
 
 return M
--- plugins/init.lua
+++ plugins/init.lua
@@ -1,25 +1,10 @@
 return {
-   {
-      "williamboman/nvim-lsp-installer",
-      config = function()
-         local lsp_installer = require "nvim-lsp-installer"
-
-         lsp_installer.on_server_ready(function(server)
-            local opts = {}
-
-            server:setup(opts)
-            vim.cmd [[ do User LspAttachBuffers ]]
-         end)
-      end,
+   ["tpope/vim-surround"] = { },
+   ["fatih/vim-go"] = { },
+   ["neovim/nvim-lspconfig"] = {
+     config = function()
+       require "plugins.configs.lspconfig"
+       require "custom.plugins.lspconfig"
+     end,
    },
-
-   {
-      "jose-elias-alvarez/null-ls.nvim",
-      after = "nvim-lspconfig",
-      config = function()
-         require("custom.null-ls-config").setup()
-      end,
-   },
-
-   { "ckipp01/stylua-nvim", run = "cargo install stylua" },
 }
