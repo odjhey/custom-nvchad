b14101446daf7c2551c10e105d3a50068e146bba
Odelon Jhey Pacalso <odjhey@gmail.com>
2022-12-04 16:42:12 +0800

    feat: update buffer and tab mapping
    final fucking lee fixed clipboard issue

--- chadrc.lua
+++ chadrc.lua
@@ -7,7 +7,6 @@ local M = {}
 
 M.options = {
    user = function()
-      vim.opt.clipboard = ""
    end,
 }
 
@@ -18,6 +17,17 @@ M.mappings = {
      ["<right>"] = {"<cmd> cnext <cr>", ""},
      ["<up>"]    = {"<cmd> lnext <cr>", ""},
      ["<down>"]  = {"<cmd> lprev <cr>", ""},
+     ["<TAB>"]   = { "<nop>", "for now, until i find a better usecase" },
+     ["<TAB>n"]   = { "<cmd> tabnext <cr>", "next tab" },
+     ["<Tab>c"] = { "<cmd> tab split <cr>", "goto prev tab" },
+     ["<Tab>x"] = { "<cmd> tabclose <cr>", "goto prev tab" },
+     ["<leader><CR>"] = { "<C-^>", "" },
+     ["<leader>n"] = {
+       function()
+         require("nvchad_ui.tabufline").tabuflineNext()
+       end,
+       "goto next buffer",
+      },
     }
   }
 }
