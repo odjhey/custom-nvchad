2e4e61552f86d91280adc64fc48c76a03cadddca
Odelon Jhey Pacalso <odjhey@gmail.com>
2023-08-18 14:16:58 +0800

    init v2

--- chadrc.lua
+++ chadrc.lua
@@ -1,60 +1,20 @@
-local M = {
-  plugins = require("custom.plugins"),
-  mappings = {
-    user = {
-      n = {
-        ["<left>"] = {
-          "<cmd> cprev <cr>",
-          ""
-        },
-        ["<right>"] = {
-          "<cmd> cnext <cr>",
-          ""
-        },
-        ["<up>"] = {
-          "<cmd> lnext <cr>",
-          ""
-        },
-        ["<down>"] = {
-          "<cmd> lprev <cr>",
-          ""
-        },
-        ["<TAB>"] = {
-          "<nop>",
-          "for now, until i find a better usecase"
-        },
-        ["<TAB>n"] = {
-          "<cmd> tabnext <cr>",
-          "next tab"
-        },
-        ["<Tab>c"] = {
-          "<cmd> tab split <cr>",
-          "goto prev tab"
-        },
-        ["<Tab>x"] = {
-          "<cmd> tabclose <cr>",
-          "goto prev tab"
-        },
-        ["<leader><CR>"] = {
-          "<C-^>",
-          ""
-        },
-        ["<leader>n"] = {
-          function()
-            return require("nvchad_ui.tabufline").tabuflineNext()
-          end,
-          "goto next buffer"
-        },
-        ["<leader>h"] = {
-          "<nop>",
-          ""
-        },
-        ["<leader>v"] = {
-          "<nop>",
-          ""
-        }
-      }
-    }
-  }
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "onedark",
+  theme_toggle = { "onedark", "one_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
 }
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
 return M
